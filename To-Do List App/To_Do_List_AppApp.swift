14d89e8478f43a94a430f889528cf2180c6d2789
Hayden Chan <hchan@lcs.on.ca>
2024-04-05 09:16:18 -0400

    Initial Commit

---
+++ To-Do List App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To-Do List App
+//
+//  Created by Hayden Chan on 2024-04-05.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
